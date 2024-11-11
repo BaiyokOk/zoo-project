37ae3779632ad8ee133a9bb56224501860f93a15
BaiyokOk <pw25082547@gmail.com>
2024-11-11 13:59:53 +0000

    Added the test cases

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,20 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
+    def test_invalid_age(self):
+        self.assertEqual(self.zoo.get_ticket_price(-3), "Invalid age")
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(11), 50)
+
+    def test_young_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(17), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(49), 150)
+
+    def test_senior_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(73), 100)
 
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if age < 0:
+            return "Invalid age" #เพิ่มเงื่อนไขเมื่อ age มีค่าน้อยกว่า 0
+        elif 0 < age <= 12:
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age < 20: # แก้จาก < 20 เป็น <= 20
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60: # แก้จาก 21 < เป็น 21 <=
             return 150
-        elif age >= 60:
+        elif age > 60: # แก้จาก >= เป็น >
             return 100
